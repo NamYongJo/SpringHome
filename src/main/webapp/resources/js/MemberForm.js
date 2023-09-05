c209e8a29354071bc9eb9d97fb3bf832ef6148af
sbm <sbm7905@gmail.com>
2023-09-05 18:13:01 +0900

    project back-up

---
+++ src/main/webapp/resources/js/MemberForm.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+ function pageMoveListFnc(){
+	 var url = "./list.do";
+	 
+	 location.href = url;
+ }
---
+++ src/main/webapp/resources/js/tempNote.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+ window.onload = function(){
+      var memberNameInputObj = document.getElementById('memberName');
+      
+      memberNameInputObj.style.backgroundColor ='#E7E7E7';
+   }
